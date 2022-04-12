541e32daba199b64ea6724e0be2f4a0a2c3d8971
Ron Lange <theincredibleron@users.noreply.github.com>
2022-04-12 10:51:52 +0200

    change adjacent detection
    changed adjacents detection for plane from spherical to box. also added option to visually debug overlap boxes

--- Programming Theory Project/Assets/Scripts/Edge.cs
+++ Programming Theory Project/Assets/Scripts/Edge.cs
@@ -1,13 +1,6 @@
-public class Edge : Reparenting
-{
-    void Awake()
-    {
-        m_SphereRadius = 0.6f;        
-    }
+using UnityEngine;
 
-    private void OnTransformParentChanged() {
-        RotationParent = transform.parent.gameObject;
-        //if (RotationParent != null && RotationParent.GetComponent<Plane>().IsRotating) return;
-        ReparentAdjacent("Corner");
-    }
+public class Edge : MonoBehaviour
+{
+
 }
--- Programming Theory Project/Assets/Scripts/Plane.cs
+++ Programming Theory Project/Assets/Scripts/Plane.cs
@@ -1,28 +1,22 @@
 using System.Collections;
 using UnityEngine;
 
-public partial class Plane : Reparenting
+public class Plane : MonoBehaviour
 {
     float m_FixedAngle = 90.0f;
-    float m_CurrentAngle = 0f;
     Quaternion m_TargetRotation;
     float m_RotationSpeed = 3.0f;
     [SerializeField] public bool IsRotating { get; protected set; }
     public Vector3 RotationAxis;
     [SerializeField] Direction m_RotationDirection;
-
-    virtual protected void Awake()
-    {
-        m_SphereRadius = 0.6f;
-    }
+    [SerializeField] Vector3 m_OverlapBox = new Vector3(3, 1, 3) / 3;
 
     public IEnumerator Rotate(Direction direction)
     {
         // exit early if already rotating...
         if (IsRotating) yield return null;
-        
-        RotationParent = gameObject;
-        ReparentAdjacent("Edge");
+        // reparent all plane pieces to this transform
+        ReparentAdjacent(gameObject.transform);
         IsRotating = true;
         Transform tempTransform = transform;
         tempTransform.Rotate(RotationAxis, m_FixedAngle * ((int)direction));
@@ -38,9 +32,44 @@ public partial class Plane : Reparenting
         // to "snap-in" properly assign calculated target rotation
         transform.rotation = m_TargetRotation;
         // reparent children to cube
-        RotationParent = null;
-        ReparentAdjacent("Edge");
+        ReparentAdjacent(transform.parent);
         // stop animation
         IsRotating = false;
+    }
+
+    void ReparentAdjacent(Transform parent)
+    {
+        Collider[] adjacentColliders = Physics.OverlapBox(transform.position, 
+            m_OverlapBox, 
+            transform.rotation * Quaternion.FromToRotation(Vector3.up, RotationAxis));
+
+        foreach (Collider adjacent in adjacentColliders) { 
+            if (adjacent.gameObject == gameObject) continue;
+            if (!tag.Equals("Center")) {
+                adjacent.gameObject.transform.SetParent(transform);
+            }
+        }
+    }
+
+    public bool overlapBoxDebug = false;
+    private void OnDrawGizmos() {
+        if (!overlapBoxDebug) return;
+        // draw local coords gizmo
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(gameObject.transform.position, gameObject.transform.up);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(gameObject.transform.position, gameObject.transform.right);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(gameObject.transform.position, gameObject.transform.forward);
+        // draw rotation axis
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(gameObject.transform.position, gameObject.transform.TransformDirection(RotationAxis));
+        // draw overlap box
+        Gizmos.color = Color.green;
+
+        Gizmos.matrix = Matrix4x4.Translate(transform.position);
+        Gizmos.matrix *= Matrix4x4.Rotate(transform.rotation);
+        Gizmos.matrix *= Matrix4x4.Rotate(Quaternion.FromToRotation(Vector3.up, RotationAxis));
+        Gizmos.DrawWireCube(Vector3.zero, m_OverlapBox * 2);
     }
 }
